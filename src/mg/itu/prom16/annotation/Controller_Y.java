a30fa99120f0dce28a5aaa2bb0c77a1dd1054368
Ramavo Harinaivo <139386090+Ramavo22@users.noreply.github.com>
2024-05-17 00:59:53 +0300

    sprint 1

---
+++ src/mg/itu/prom16/annotation/Controller_Y.java
@@ -0,0 +1,10 @@
+package mg.itu.prom16.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE) // Cible les classes (et éventuellement les interfaces)
+public @interface Controller_Y{}
---
+++ src/mg/itu/prom16/utilities/Function.java
@@ -0,0 +1,67 @@
+package mg.itu.prom16.utilities;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import mg.itu.prom16.annotation.Controller_Y;
+
+public class Function {
+    // control des annotations
+    public static Boolean isAnnoted(Object obj,Class<? extends Annotation> annotation){
+        return obj.getClass().isAnnotationPresent(annotation);
+    }
+    
+    public static Boolean isController_Y(Object obj){
+        return Function.isAnnoted(obj, Controller_Y.class);
+    }
+
+    public static Path getPathProject(String dossier){
+        String repertoireTravail = System.getProperty("user.dir");
+        Path pathRepertoireTravail = Paths.get(repertoireTravail);
+        Path cheminDossierRessource = pathRepertoireTravail.resolve(dossier);
+        return cheminDossierRessource;
+    }
+
+    // Liste de controller dans un packages
+
+    public static List<Class<?>> ScanPackage(String packageName) throws IOException, ClassNotFoundException {
+        List<Class<?>> classes = new ArrayList<>();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String path = packageName.replace('.', '/');
+        
+        Enumeration<URL> resources = classLoader.getResources(path);
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            if (resource.getProtocol().equals("file")) {
+                File directory = new File(URLDecoder.decode(resource.getFile(), "UTF-8"));
+                if (directory.exists() && directory.isDirectory()) {
+                    File[] files = directory.listFiles();
+                    for (File file : files) {
+                        if (file.isFile() && file.getName().endsWith(".class")) {
+                            String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
+                            Class<?> clazz = Class.forName(className);
+                            if (clazz.isAnnotationPresent(Controller_Y.class)) {
+                                classes.add(clazz);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return classes;
+    }
+
+   
+
+
+
+    
+}
