cc427195871b2d37cb1e9d185cbcd389a4228bbb
Ramavo Harinaivo <nyxrafaly@gmail.com>
2024-06-14 23:26:49 +0300

    Sprint5
    Ajout execption et une partie de sprint 6

--- src/mg/itu/prom16/Controller/FrontController.java
+++ src/mg/itu/prom16/Controller/FrontController.java
@@ -2,7 +2,7 @@ package mg.itu.prom16.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
@@ -11,7 +11,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import mg.itu.prom16.utilities.Function;
 import mg.itu.prom16.utilities.Mapping;
 import mg.itu.prom16.utilities.ModelView_Y;
@@ -21,19 +20,30 @@ public class FrontController extends HttpServlet{
     HashMap<String, Mapping> urlpattern;
     
     public void init() throws ServletException {
-        super.init();
-        findController();
+            super.init();
+            findController();
+            
     }
-    public void findController(){
-        try {
-            liste = Function.ScanPackage(getInitParameter("controllerName"));  
-            urlpattern = Function.getUrlController(liste);
-              
-        } catch (Exception e) {
-            e.printStackTrace();
-            liste = new ArrayList<>();
+    
+    public void findController()throws ServletException{
+        String packageName= getInitParameter("controllerName");
+        // raha diso anarana lery amle web.xml
+        if(packageName == null){
+            throw new ServletException("verifier le Web.xml, parameter name doit etre controllerName");
         }
-        
+        else{
+            try {
+                liste = Function.ScanPackage(packageName);
+                urlpattern = Function.getUrlController(liste);
+            
+            }
+            catch(ServletException es){
+                throw es;
+            }
+             catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
     
     
@@ -46,21 +56,30 @@ public class FrontController extends HttpServlet{
         processRequest(req, resp);
     }
 
+
     protected void processRequest(HttpServletRequest req,HttpServletResponse resp) throws ServletException,IOException{
         resp.setContentType("text/plain");
         PrintWriter out = resp.getWriter();
         String[] listeUrl=req.getRequestURI().split("/");
+        Enumeration<String> parameterNames = req.getParameterNames();
+        HashMap<String,String> parameterValue = new HashMap<>();
+        while (parameterNames.hasMoreElements()) {
+            String paramName = parameterNames.nextElement();
+            parameterValue.put(paramName, req.getParameter(paramName));
+        }
         if(listeUrl.length < 3){
             out.println("choisir le controller");
         }
         else{
             Mapping m = urlpattern.get(listeUrl[2]);
+            
             if(m == null){
-                out.println("Tsy misy ltyh ah");
+                throw new ServletException("Tsy misy ilay url "+listeUrl[2]);
             }
             else{
                 try {
-                    Object val = Function.executeMethode(m);
+                    Object val = Function.executeMethode(m,parameterValue);
+                    // si String ou ModelView_Y
                     if(val instanceof String){
                         out.println(val); // Utilisez out.println pour envoyer la réponse au client
                     }
@@ -71,9 +90,12 @@ public class FrontController extends HttpServlet{
                         }
                         req.getRequestDispatcher(modelView_Y.getUrl()).forward(req, resp);
                     }
+                    // si Tsy String na ModelView_Y
                     if(!(val instanceof ModelView_Y) && !(val instanceof String)){
                         out.print("Na String na ModelView_Y ny class ampiasaina");
                     }
+
+
                 } catch (Exception e) {
                     e.printStackTrace(); // Utilisez out.println pour envoyer la réponse au client
                 }
