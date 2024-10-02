c7327398402b4a9f2dd2733253ea5f26f2d04960
Ramavo Harinaivo <nyxrafaly@gmail.com>
2024-10-02 22:05:38 +0300

    Sprint-10

--- src/mg/itu/prom16/annotation/Post.java
+++ src/mg/itu/prom16/annotation/Post.java
@@ -1,12 +1,12 @@
 package mg.itu.prom16.annotation;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.annotation.ElementType;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD) // Cible les classes (et éventuellement les interfaces)
-public @interface Get_Y {
-    String url();
+@Target(ElementType.METHOD)
+public @interface Post {
+    
 }
